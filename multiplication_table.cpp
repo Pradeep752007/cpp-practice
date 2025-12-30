3fb06268bf8bf016dc79983e1be1389c30e83ebd
Pradeep752007 <pradeep752007@gmail.com>
2025-12-30 21:43:00 +0530

    Create multiplication_table.cpp
    This is a program using for loop to print the multiplication table for any number

---
+++ multiplication_table.cpp
@@ -0,0 +1,16 @@
+// This is a program using for loop to print the multiplication table for any number 
+#include<iostream>
+
+using namespace std;
+
+int main(){
+  int f;  // The variable f is used to get input from the user 
+  cout<<"Enter the number for which multiplication table you want: ";
+  cin>>f;
+  cout<<"Multiplication table for "<<f<<endl;
+  for (int i = 1; i <= 10; i++)
+  {
+      cout<<f<<"*"<<i<<"="<<f*i<<endl;
+  }
+  return 0;
+}
